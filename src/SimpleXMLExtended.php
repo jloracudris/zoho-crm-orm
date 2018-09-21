9ee6728607ce45bc8c0aaa7a87f129e01d1f73a2
Jose Lora <jloracudris@gmail.com>
2018-09-20 23:02:46 -0500

    JL

--- src/SimpleXMLExtended.php
+++ src/SimpleXMLExtended.php
@@ -1,9 +1,16 @@
 <?php 
+namespace Wabel\Zoho\CRM;
 class SimpleXMLExtended extends SimpleXMLElement {
-  public function addCData($cdata_text) {
-    $node = dom_import_simplexml($this); 
-    $no   = $node->ownerDocument; 
-    $node->appendChild($no->createCDATASection($cdata_text)); 
-  } 
+  public function addCData($name, $value = NULL) {
+    $new_child = $this->addChild($name);
+
+    if ($new_child !== NULL) {
+      $node = dom_import_simplexml($new_child);
+      $no   = $node->ownerDocument;
+      $node->appendChild($no->createCDATASection($value));
+    }
+
+    return $new_child;
+  }  
 }
 ?>
